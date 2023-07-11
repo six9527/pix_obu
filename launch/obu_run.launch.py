185c4595b02c6c554ca2ca9e11d83784b3ef2521
six9527 <487844521@qq.com>
2023-07-11 20:49:46 +0800

    init

---
+++ launch/obu_run.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription           # launch文件的描述类
+from launch_ros.actions import Node            # 节点启动的描述类
+
+def generate_launch_description():             # 自动生成launch文件的函数
+    return LaunchDescription([                 # 返回launch文件的描述信息
+        Node(                                  # 配置一个节点的启动
+            package='obu',          # 节点所在的功能包
+            executable='AbnormalInformationUpload', # 节点的可执行文件
+        ),
+       Node(                                  # 配置一个节点的启动
+            package='obu',          # 节点所在的功能包
+            executable='RealTimeInformationUpload', # 节点的可执行文件
+        ),
+        Node(                                  # 配置一个节点的启动
+            package='obu',          # 节点所在的功能包
+            executable='RegistratIonInformationReport', # 节点的可执行文件
+        ),
+        Node(                                  # 配置一个节点的启动
+            package='obu',          # 节点所在的功能包
+            executable='StatusInformationUpload', # 节点的可执行文件
+        ),
+        Node(                                  # 配置一个节点的启动
+            package='obu',          # 节点所在的功能包
+            executable='tcp_socket_node', # 节点的可执行文件
+        ),
+    ])
